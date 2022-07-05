5004c81342563b7ec31f0fa6c2db1b208639bc9f
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-05 11:32:51 +0000

    5章終了6章スタート

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,8 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
     redirect_to '/top'
   end
 
@@ -21,14 +18,14 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
-  def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
   end
-
 
   private
   def list_params
